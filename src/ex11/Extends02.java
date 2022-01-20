84c105112c137a66d2f867ef6bd2cc8f51c433a7
JY <JY@naver.com>
2022-01-21 00:32:31 +0900

    로또프로그램

--- src/ex11/Extends02.java
+++ src/ex11/Extends02.java
@@ -2,20 +2,20 @@ package ex11;
 
 class 동물 {
 
-    String getName() {
+    String name() {
         return "";
     };
 
-    int getHp() {
+    int hp() {
         return 0;
     }
 
-    int getAttack() {
+    int attack() {
         return 0;
     }
 
-    void setHp(int hp) {
-
+    void demagehp(int a, int b) {
+        ;
     }
 }
 
@@ -24,20 +24,20 @@ class 사자 extends 동물 {
     int hp = 100;
     int attack = 10;
 
-    String getName() {
+    String name() {
         return name;
     };
 
-    int getHp() {
+    int hp() {
         return hp;
     }
 
-    int getAttack() {
+    int attack() {
         return attack;
     }
 
-    void setHp(int hp) {
-        this.hp = hp;
+    void demagehp(int a, int b) {
+        hp = a - b;
     }
 }
 
@@ -46,20 +46,20 @@ class 호랑이 extends 동물 {
     int hp = 100;
     int attack = 15;
 
-    String getName() {
+    String name() {
         return name;
     };
 
-    int getHp() {
+    int hp() {
         return hp;
     }
 
-    int getAttack() {
+    int attack() {
         return attack;
     }
 
-    void setHp(int hp) {
-        this.hp = hp;
+    void demagehp(int a, int b) {
+        hp = a - b;
     }
 }
 
@@ -68,42 +68,20 @@ class 곰 extends 동물 {
     int hp = 100;
     int attack = 50;
 
-    String getName() {
+    String name() {
         return name;
     };
 
-    int getHp() {
+    int hp() {
         return hp;
     }
 
-    int getAttack() {
+    int attack() {
         return attack;
     }
 
-    void setHp(int hp) {
-        this.hp = hp;
-    }
-}
-
-class 늑대 extends 동물 {
-    String name = "늑대";
-    int hp = 70;
-    int attack = 70;
-
-    String getName() {
-        return name;
-    };
-
-    int getHp() {
-        return hp;
-    }
-
-    int getAttack() {
-        return attack;
-    }
-
-    void setHp(int hp) {
-        this.hp = hp;
+    void demagehp(int a, int b) {
+        hp = a - b;
     }
 }
 
@@ -112,19 +90,19 @@ public class Extends02 {
     // 오버라이딩 = 무효화하다 ->아래로 타고 내려가는 기법
     // 사자 -> 호랑이 공격
     static void attack(동물 unit1, 동물 unit2) {
-        System.out.println(unit1.getName() + "가(이) " + unit2.getName() + "를(을) 공격하고 있습니다.");
-        unit2.setHp(unit2.getHp() - unit1.getAttack());
-        System.out.println(unit2.getName() + "의 hp : " + unit2.getHp());
+        System.out.println(unit2.name() + "가(이) 공격당하고 있습니다.");
+        // unit2.hp() = unit2.hp() - unit1.attack() <= 메서드를 호출한것 변수가 아니라 값을 변경할수 없기 때문에
+        // 오류가남
+        unit2.demagehp(unit2.hp(), unit1.attack());
+        System.out.println(unit2.name() + "의 hp : " + unit2.hp());
     }
 
     public static void main(String[] args) {
         동물 lion = new 사자();
         동물 tiger = new 호랑이();
         동물 bear = new 곰();
-        동물 wolf = new 늑대();
 
         attack(lion, tiger);
         attack(lion, bear);
-        attack(wolf, lion);
     }
 }
--- src/ex22/CollectionEx04.java
+++ src/ex22/CollectionEx04.java
@@ -5,45 +5,77 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 public class CollectionEx04 {
     public static void main(String[] args) {
+        // 사용 함수들
         Scanner sc = new Scanner(System.in);
-        // int money = sc.nextInt();
-        // System.out.println("받은 금액 : " + money + "원");
-        // int chance = money / 1000;
-        // System.out.println("기회 : " + chance + "번");
-        Random r = new Random();
-        ArrayList<Integer> lotto = new ArrayList<Integer>();
+        Random r = new Random(); // 랜덤 메서드
+        int num, same = 0; // 랜덤으로 들어가는 숫자, 맞춘횟수
+        HashSet<Integer> setLotto = new HashSet<Integer>(); // 당첨 번호
+        ArrayList<ArrayList<Integer>> chanceLotto = new ArrayList<ArrayList<Integer>>(); // 기회
+        HashSet<Integer> pickLotto = new HashSet<Integer>(); // 뽑은 번호
+
+        // 1000원당 기회 1번 부여
+        int money = sc.nextInt(); // 투입금액
+        System.out.println(); // 줄 뛰우기
+        System.out.println("받은 금액 : " + money + "원");
+        int chance = money / 1000; // 투입금액당 기회수
+        System.out.println("기회 : " + chance + "번");
+
+        // 당첨 번호 구하기.
         while (true) {
-            int num = r.nextInt(45) + 1;
-            lotto.add(num);
-            if (lotto.size() > 1) {
-                for (int j = 0; j < lotto.size(); j++) {
-                    for (int i = lotto.size() - 1; i == 0; i--) {
-                        if (lotto.get(j) == lotto.get(i - j)) {
-                            lotto.remove(i);
-                        }
+            num = r.nextInt(45) + 1;
+            setLotto.add(num);
+            if (setLotto.size() == 6) {
+                break;
+            }
+        }
+
+        // 자동으로 번호 뽑기
+        while (true) {
+            while (true) {
+                num = r.nextInt(45) + 1;
+                pickLotto.add(num);
+                if (pickLotto.size() == 6) {
+                    break;
+                }
+            }
+            ArrayList<Integer> pick = new ArrayList<>(pickLotto); // 뽑은 번호를 ArrayList로 변환
+            Collections.sort(pick); // 뽑은 번호를 정렬
+            chanceLotto.add(pick); // 기회 1회 소모. => 뽑은 번호를 기회에 넣음 즉 1줄완성.
+            if (chanceLotto.size() == chance) { // 기회수가 다되므로 break;
+                break;
+            }
+            pickLotto = new HashSet<Integer>(); // 한줄 더 생성.
+        }
+
+        // 번호 출력
+        ArrayList<Integer> lotto = new ArrayList<>(setLotto); // 당첨 번호 ArrayList로 변환
+        Collections.sort(lotto); // // 당첨 번호 크기순으로 정렬
+        System.out.println(); // 줄 뛰우기
+        System.out.println("뽑은 번호 : " + chanceLotto); // 뽑은 번호 모두 출력
+        System.out.println(); // 줄 뛰우기
+        System.out.println("당첨 번호 : " + lotto); // 당첨 번호 모두 출력
+
+        // 번호 비교
+        for (int i = 0; i < chanceLotto.size(); i++) { // i = 기회
+            System.out.println(); // 줄 뛰우기
+            System.out.println(i + 1 + "번째 기회");
+            for (int j = 0; j < 6; j++) { // j = 로또 당첨 번호
+                for (int k = 0; k < 6; k++) { // k = 내가 뽑은 번호
+                    if (lotto.get(j) == chanceLotto.get(i).get(k)) { // lotto.get(j) = 로또 당첨번호, chanceLotto.get(i) = 기회,
+                                                                     // chanceLotto.get(i).get(k) = 내가 뽑은 번호
+                        same++; // 당첨번호와 뽑은번호가 맞으면 맞춘갯수 1씩 증가
                     }
                 }
             }
-            if (lotto.size() == 6) {
-                break;
+            if (same > 0) { // 맞춘갯수가 1이상 있으면
+                System.out.println(same + "개 맞았습니다.");
+            } else { // 맞춘갯수가 0이면
+                System.out.println("꽝입니다");
             }
+            same = 0; // 한번의 기회가 끝나면 맞춘갯수 초기화
         }
-        Collections.sort(lotto);
-        HashSet<Integer> pick = new HashSet<Integer>();
-        while (true) {
-            int num = r.nextInt(45) + 1;
-            pick.add(num);
-            if (pick.size() == 6) {
-                break;
-            }
-        }
-        TreeSet<Integer> myTreeSet = new TreeSet<Integer>();
-        myTreeSet.addAll(pick);
-        System.out.println(pick);
-        System.out.println(lotto);
     }
 }
